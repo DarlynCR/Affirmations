1ec8b257b0113fccc411d318ac488bd6f70b4326
DarlynCR <darlyn.caicedo.calderon@gmail.com>
2022-06-20 17:26:42 -0500

    Se configura recyclerView para mostrar una lista desplazable

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding  = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //se iniciliza la lista de los datos a pintar en el rv
+        val myDataset = Datasource().loadAffirmations()
+        //Inicializa el rv
+        val recyclerView = binding.rvAffirmations
+        //instancio el adapter a pasar en rv y s ele pasa el context y la lista de datos
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        //Si el diseño es fijo, para mejorar el rendimiento
+        recyclerView.setHasFixedSize(true)
+        //Úsala si sabes que los cambios en el contenido no cambiarán el tamaño del diseño de RecyclerView.
     }
 }
---
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+//ItemAdapter necesita información sobre cómo resolver los recursos de strings. Esta y otra información sobre la app se almacena en una instancia del objeto Context
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    //El layout manager llama al método onCreateViewHolder() a fin de crear nuevas interfaces de vista
+    // para RecyclerView (cuando no hay viewHolder existentes que puedan reutilizarse).
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // crea una nueva vista
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+    // El layout manager llama a este método para reemplazar el contenido de una vista de elementos de lista.
+    //En este método, encontrarás el objeto Affirmation adecuado del conjunto de datos según la posición.
+    override fun onBindViewHolder(holder: ItemAdapter.ItemViewHolder, position: Int) {
+        //ItemViewHolder, creado anteriormente por el método onCreateViewHolder(),
+        //int que representa el elemento actual position en la lista.
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+
+    }
+
+    //El método getItemCount() debe mostrar el tamaño de tu conjunto de datos.
+    override fun getItemCount() = dataset.size
+
+    // Como ItemAdapter solo usa ItemViewHolder, si se crea dentro de ItemAdapter, se muestra esta relación. Esto no es obligatorio, pero ayuda a otros desarrolladores a comprender la estructura de tu programa.
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+}
